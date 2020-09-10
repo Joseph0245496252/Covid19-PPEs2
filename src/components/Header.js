5f8bacb58dff3b53416dbdf930a27a118d294a0b
Joseph Asante <freecoding1@gmail.com>
2020-09-10 14:14:00 +0000

    final project redered successfully

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,66 +2,78 @@ import React from "react";
 
 function Header() {
   return (
-      <div className="container-fluid">
-        <div className="row carouselrow">
-          <div className="col">
-            {/* carousel */}
-            <div
-              id="carouselExampleFade"
-              class="carousel slide carousel-fade"
-              data-ride="carousel"
-            >
-              <div className="carousel-inner code01">
-                <div className="carousel-item active">
-                  <img
-                    src={require("../images/code01.png")}
-                    className="d-block w-100 carouselimg1"
-                    alt="..."
-                  />
-                </div>
-                <div className="carousel-item code02">
-                  <img
-                    src={require("../images/code02.png")}
-                    className="d-block w-100 carouselimg1"
-                    alt="..."
-                  />
-                </div>
-                <div className="carousel-item code03">
-                  <img
-                    src={require("../images/code03.png")}
-                    className="d-block w-100 "
-                    alt="..."
-                  />
-                </div>
+    <div className="container-fluid">
+    <div className="row carouselrow">
+      <div className="col">
+        {/* carousel */}
+        <div
+          id="carouselExampleFade"
+          class="carousel slide carousel-fade"
+          data-ride="carousel"
+        >
+           <div class="carousel-caption d-none d-md-block carousel-text">
+                <h5>Covid 19 health tips</h5>
+                <p>Protect yourself and others from the spread COVID-19.</p>
               </div>
-              <a
-                className="carousel-control-prev"
-                href="#carouselExampleFade"
-                role="button"
-                data-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a
-                className="carousel-control-next"
-                href="#carouselExampleFade"
-                role="button"
-                data-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="sr-only">Next</span>
-              </a>
+          <div className="carousel-inner code01">
+            <div className="carousel-item active">
+              <img
+                src={require("../images/code01.png")}
+                className="d-block w-100 carouselimg1"
+                alt="..."
+              />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Dilevering makes faster and better</h5>
+                <p>We dilevery at your door post.</p>
+              </div>
+            </div>
+            <div className="carousel-item code02">
+              <img
+                src={require("../images/code02.png")}
+                className="d-block w-100 carouselimg1"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item code03">
+              <img
+                src={require("../images/code03.png")}
+                className="d-block w-100 "
+                alt="..."
+              />
+             <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Personal Protective Equipment (PPE).</p>
+              </div>
             </div>
           </div>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleFade"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleFade"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
         </div>
       </div>
+    </div>
+  </div>
 
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,60 +4,60 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="container-fluid">
-    <nav className="navbar fixed-top navbar-expand-md navbar-dark scrolling-navbar">
-      <Link className="navbar-brand" hred="#"><strong className="navbar-item">Covid 19 PPEs</strong></Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#sticky-nav">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse " id="sticky-nav">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item navbarSection">
-            <Link href="#" className="nav-link active"><strong className="navbar-item">Home</strong></Link>
-          </li>
-          <li className="navbar-item">
-            <Link href="#" className="nav-link"><strong className="navbar-item">About</strong></Link>
-          </li>
-          <li className="navbar-item">
-            <Link href="#" className="nav-link"><strong className="navbar-item">Service</strong></Link>
-          </li>
-          <li className="navbar-item">
-            <Link href="#" className="nav-link"><strong className="navbar-item">Contact</strong></Link>
-          </li>
-          
-        </ul>
-      </div>
-    </nav>
-    <div className="view">
-      {/* <div className="flex-center">
+      <nav className="navbar fixed-top navbar-expand-md navbar-dark scrolling-navbar">
+        <Link className="navbar-brand" hred="#">
+          <strong className="navbar-item">Covid 19 PPEs</strong>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#sticky-nav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse " id="sticky-nav">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item navbarSection">
+              <Link href="#" className="nav-link active">
+                <strong className="navbar-item">Home</strong>
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link href="#" className="nav-link">
+                <strong className="navbar-item">About</strong>
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link href="#" className="nav-link">
+                <strong className="navbar-item">Service</strong>
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link href="#" className="nav-link">
+                <strong className="navbar-item">Contact</strong>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="view">
+        {/* <div className="flex-center">
         <Link href="#scroll" className="btn-primary">Scrll Down</Link>
       </div> */}
-    </div>
-    
-    <div className="container-fluid">
+      </div>
+
+      <div className="container-fluid">
         <div className="row bold">
           <div className="col-md-12">
             <b align="justify">
-            We are delivering to your door post with limited shipping options. Please expect extended delivery time.
+              We are delivering to your door post with limited shipping options.
+              Please expect extended delivery time.
             </b>
-            
           </div>
         </div>
       </div>
-    {/* <main className="text-center py-5" id="scroll"> 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h6 align="justify">
-            We are delivering to your door post with limited shipping options. Please expect extended delivery time.
-            </h6>
-            
-          </div>
-        </div>
-      </div>
-    </main>  */}
-   
     </div>
-    
   );
 }
 
